7585b2b4cd188a1ae707913de70e6006bf70cacd
Sophie Gu <sophieqgu@gmail.com>
2020-02-05 22:45:54 +0000

    Automatically backed up by Learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -7,5 +7,10 @@ class StudentsController < ApplicationController
   def show
     @student = Student.find(params[:id])
   end
+  
+  def new 
+  end 
 
+  def create 
+    @student = Student.create
 end
