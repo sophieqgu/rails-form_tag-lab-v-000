6a1365ccd797dbda0c0523218fec27dae0ba4c8b
Sophie Gu <sophieqgu@gmail.com>
2020-02-05 22:46:42 +0000

    Automatically backed up by Learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -12,5 +12,7 @@ class StudentsController < ApplicationController
   end 
 
   def create 
-    @student = Student.create
+    @student = Student.create(params[:student])
+    redirect_to student_path
+  end 
 end
