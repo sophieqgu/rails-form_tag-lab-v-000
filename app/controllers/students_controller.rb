8c43ba3a72a168e7044e060f6d635132f680353b
Sophie Gu <sophieqgu@gmail.com>
2020-02-05 23:43:20 +0000

    Automatically backed up by Learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -12,7 +12,7 @@ class StudentsController < ApplicationController
   end 
 
   def create 
-    @student = Student.create(params[:student])
+    Student.create(:first_name => params[:student][:first_name], :last_name => params[:student][:last_name])
     redirect_to new_student_path
   end 
 end
